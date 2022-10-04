8c2a25f3b6aa68186ca2827ef5ec756739c1899f
Lyn722 <113714237+Lyn722@users.noreply.github.com>
2022-10-03 20:21:53 -0500

    Add files via upload

---
+++ Lab04.java
@@ -0,0 +1,25 @@
+package lab04;
+
+public class Lab04 {
+
+
+    public static void main (String[] args) {
+        
+        int[] array = { 45, 67, 23, 50, 1, 505, 54, 40, 90};
+        System.out.println(search(array, 54));
+    }  
+        
+    public static int search(int[] x, int key){
+        for(int i=0; i<x.length;i++){
+            if(x[i]  == key)
+                return i;
+        } 
+              return -1;
+        }
+      
+        
+        
+        
+    
+    
+}
